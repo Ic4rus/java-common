c8bc3633aaaf8be19c5c93ffc1ca831e09194e55
Ic4rus <thanght4391@gmail.com>
2019-07-08 15:44:13 +0700

    Regular expression constants

---
+++ src/main/java/com/icarus/jc/string/RegexConstants.java
@@ -0,0 +1,9 @@
+package com.icarus.jc.string;
+
+public interface RegexConstants {
+
+    public static final String PHONE_REGEX = "(\\+[0-9]+[\\- \\.]*)?(\\([0-9]+\\)[\\- \\.]*)?([0-9][0-9\\- \\.]+[0-9])";
+
+    public static final String FULL_NAME_REGEX = "^[a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ"
+            + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s]+$";
+}
